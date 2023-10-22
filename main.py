041e09fc9d14f9c5381ba6aa3bdd0735c41b0007
Pavel <139257888+n0omik@users.noreply.github.com>
2023-10-22 09:19:53 +0400

    v0.12 function btc_price fix

--- main.py
+++ main.py
@@ -3,34 +3,29 @@ import requests
 from aiogram import Bot, Dispatcher, executor, types
 
 key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-
-# Making list for multiple crypto's 
-currencies = ["BTCUSDT"] 
-
   
-# running loop to print all crypto prices
+# running function to print btc crypto price
 def currencies_price(currencies):
-   j = 0
-   for i in currencies: 
-    # completing API for request 
+    currencies = ["BTCUSDT"] 
     url = key+currencies[j]   
     data = requests.get(url) 
     data = data.json() 
-    j = j+1
     return(f"{data['symbol']} price is {data['price']}") 
 
-
+#API TOKEN for telegram bot n0omik
 API_TOKEN = '6783402247:AAGLMtEeDpsKGgxY9PiT3BTxn-N4butoQ-k'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+#finction of start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
    await message.reply("Привет, я бот от Нумика, помогаю торговать на криптовалюте")
 
+#finction of btc price
 @dp.message_handler(commands=['Bitcoin_price'])
-async def echo(message: types.Message):
-   await message.answer(currencies_price)
+async def btc_price(message: types.Message):
+   await message.reply(currencies_price)
  
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)

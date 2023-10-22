d8d63d23bdbbc9069d73ec04e0b547fdfc09cdb7
Pavel <139257888+n0omik@users.noreply.github.com>
2023-10-22 09:08:17 +0400

    v0.1 bitcoin prices parsing

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import json 
+import requests 
 from aiogram import Bot, Dispatcher, executor, types
+
+key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+# Making list for multiple crypto's 
+currencies = ["BTCUSDT"] 
+
+  
+# running loop to print all crypto prices
+def currencies_price(currencies):
+   j = 0
+   for i in currencies: 
+    # completing API for request 
+    url = key+currencies[j]   
+    data = requests.get(url) 
+    data = data.json() 
+    j = j+1
+    print(f"{data['symbol']} price is {data['price']}") 
+
+
 API_TOKEN = '6783402247:AAGLMtEeDpsKGgxY9PiT3BTxn-N4butoQ-k'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -7,9 +28,9 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
    await message.reply("Привет, я бот от Нумика, помогаю торговать на криптовалюте")
 
-@dp.message_handler()
+@dp.message_handler(commands=['Bitcoin_price'])
 async def echo(message: types.Message):
-   await message.answer(message.text)
+   await message.answer(currencies_price)
  
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)

e55d2c73580e5e8b2a35d34f9d00faaef12cf02c
Pavel <139257888+n0omik@users.noreply.github.com>
2023-10-22 09:50:25 +0400

    v0.14 tired of fixing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import json 
 import requests 
+import asyncio
+import logging
+import sys
+
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import Message
+
 
 currencies = ["BTCUSDT"] 
   
-# running function to print btc crypto price
+# function of printing btc price
 def currencies_price(currencies):
     for currency in currencies:
       key = f"https://api.binance.com/api/v3/ticker/price?symbol={currency}"
@@ -17,15 +23,15 @@ API_TOKEN = '6783402247:AAGLMtEeDpsKGgxY9PiT3BTxn-N4butoQ-k'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-#finction of start
+#function of start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
    await message.reply("Привет, я бот от Нумика, помогаю торговать на криптовалюте")
 
-#finction of btc price
+#function of btc price
 @dp.message_handler(commands=['Bitcoin_price'])
 async def btc_price(message: types.Message):
-   await message.reply(currencies_price)
+   await message.reply('currencies_price')
  
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)

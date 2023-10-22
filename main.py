5c5be6fbb05adff0bacdb9227cac94d16d7a64d1
Pavel <139257888+n0omik@users.noreply.github.com>
2023-10-22 10:07:57 +0400

    v 0.15 still not working function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import sys
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import Message
 
 
@@ -26,12 +27,19 @@ dp = Dispatcher(bot)
 #function of start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-   await message.reply("Привет, я бот от Нумика, помогаю торговать на криптовалюте")
+   kb = [
+       [
+           types.KeyboardButton(text="Сможешь повторить это?"),
+           types.KeyboardButton(text="А это?")
+       ],
+   ]
+   keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+   await message.reply("Привет, я бот от Нумика, помогаю торговать на криптовалюте", reply_markup=keyboard)
 
 #function of btc price
 @dp.message_handler(commands=['Bitcoin_price'])
 async def btc_price(message: types.Message):
-   await message.reply('currencies_price')
+   await message.reply(currencies_price)
  
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)
